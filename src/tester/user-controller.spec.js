99d28df34f96bf16d2de75b1b72fe654969b43e2
rayfarandi <43269555+rayfarandi@users.noreply.github.com>
2024-02-28 18:00:11 +0700

    update

--- src/tester/user-controller.spec.js
+++ src/tester/user-controller.spec.js
@@ -1,33 +1,31 @@
 
-const { describe, it } = require("mocha")
-const { expect } = require("chai")
+const { describe, it } = require('mocha')
+const { expect } = require('chai')
 
 const userController = require('../controllers/admin/user.controller')
 
 const req = {
     query:{}
 }
-
 const res = {
-    status: (status) => {
+    status: (status)=>{
         return res
-    },json:(param)=>{
-        return param
-    }
+    }, json :(params)=>{
+        return params
+    },
 }
 
 describe('list all user',()=>{
     it('should return success true', async()=>{
-
         const response= await userController.getAllUsers(req,res)
-        expect(response.success).to.bee.eq(true)
+        expect(response.success).to.be.eq(true)
     })
-    it('next should be null', async()=>{
+    it('nextPage should be null', async()=>{
         const req = {
-            query:{ page: '7' }
+            query:{ page: '1' }
         }
         const response = await userController.getAllUsers(req,res)
-        expect(response.pageInfo.nextPage).to.be.eq(null)
+        expect(response.pageInfo.nextPage).to.be.eq(2)
     })
     it('prevPage should be null',async()=>{
         const req = {
@@ -41,7 +39,89 @@ describe('list all user',()=>{
             query:{ searchKey: 'tidak ada' }
         }
         const response = await userController.getAllUsers(req,res)
-        console.log(response)
     })
 })
 
+describe('detail user',()=>{
+    const req={
+        params:{id:1}
+    }
+    const res = {
+        status: (status) => {
+            return res;
+        },
+        json: (params) => {
+            return params;
+        }
+    }
+    
+    it('should return success true',async()=>{
+        const response=await userController.getDetailUser(req,res)
+        expect(response.success).to.be.eq(true)
+    })
+    it('user not found',async()=>{
+        const req={
+            params:{id:10000}
+        }
+        const response = await userController.getDetailUser(req,res)
+        expect(response.success).to.be.eq(false)
+        expect(response.message).to.be.eq('user not found')
+
+    })
+
+    it('should return error when an error occurs', async () => {
+        const req = {
+            params: { id: 'invalidId' } 
+        };
+        const response = await userController.getDetailUser(req, res);
+        expect(response.success).to.be.eq(false);
+        expect(response.message).to.be.eq('Internal server error');
+    });
+})
+
+
+describe('create user', () => {
+    
+    it('should return success true', async() => {
+        const req = {
+            body: {
+                fullName: "testing",
+                email: "testing9999@mail.com",
+                password: "1234",
+            }
+        }
+        const response = await userController.createUser(req, res)
+        expect(response.success).to.be.eq(true)
+    })
+
+
+    it('should return message email esting9999@mail.com already registered', async() => {
+        const req = {
+            body: {
+                fullName: "testing",
+                email: "testing9999@mail.com",
+                password: "1234",
+            }
+        }
+        const response = await userController.createUser(req, res)
+        expect(response.message).to.be.eq("testing9999@mail.com already exist")
+    })
+})
+
+describe('delete user', () => {
+    const req = {
+        params: {id: 10}
+    }
+
+    it('should return success true', async() => {
+        const response = await userController.deleteUser(req, res)
+        expect(response.success).to.be.eq(true)
+    })
+
+    it('should throw not found', async() => {
+        const req = {
+            params: {id: 10}
+        }
+        const response = await userController.deleteUser(req, res)
+    })
+})
